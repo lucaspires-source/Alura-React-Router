32aec243b82212c300f6140085b20bb4f7c9c5bf
lucaspires-source <pires.lucas94@gmail.com>
2021-04-03 11:27:46 -0300

    Aula 04.04 Post Inexistente

--- src/paginas/Post.js
+++ src/paginas/Post.js
@@ -1,12 +1,16 @@
 import React, { useState,useEffect } from 'react'
 import { busca } from '../api/api'
 import '../assets/css/post.css'
-import {useParams} from 'react-router-dom'
+import {useParams, useHistory} from 'react-router-dom'
 const Post = ({}) => {
+    let history = useHistory()
     const {id} = useParams()
     const [post, setPost] = useState({})
     useEffect(() =>{
         busca(`/posts/${id}`,setPost)
+        .catch(()=>{
+            history.push("/404")
+        })
     },[id])
     return (
         <main className="container flex flex--centro">
