ae7b29e7c700295d43e51c5f8ca747a84f086e89
lucaspires-source <pires.lucas94@gmail.com>
2021-04-03 09:29:12 -0300

    Aula 02.05

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React from 'react'
-import './assets/css/base/base.css'
-import Home from './paginas/Home';
-import Sobre from './paginas/Sobre';
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
-import Pagina404 from './paginas/Pagina404';
-
+import React from "react";
+import "./assets/css/base/base.css";
+import Home from "./paginas/Home";
+import Sobre from "./paginas/Sobre";
+import { BrowserRouter, Router, Route, Switch, Link } from "react-router-dom";
+import Pagina404 from "./paginas/Pagina404";
+import Cabecalho from "./components/Cabecalho";
 
 const App = () => {
-
   return (
     <BrowserRouter>
+      <Cabecalho />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/sobre" component={Sobre} />
-        <Route path="" component={Pagina404}/>
+        <Route component={Pagina404} />
       </Switch>
     </BrowserRouter>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
