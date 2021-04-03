27d1c61c6a71535f76961093076d111bc2b80753
lucaspires-source <pires.lucas94@gmail.com>
2021-04-03 10:15:41 -0300

    Aula 03.03

--- src/components/Cabecalho.js
+++ src/components/Cabecalho.js
@@ -9,17 +9,17 @@ const Cabecalho = () => {
                 <span className="menu-hamburguer__icone"></span>
             </div>
             <div className="cabecalho-container">
-                <a href="/" className="flex flex--centro">
+                <Link to="/" className="flex flex--centro">
                     <img  className="cabecalho__logo" src={image}/>
                     <h1 className="cabecalho__titulo">Petshop</h1>
-                </a>
+                </Link>
             </div>
             <nav className="menu-cabecalho">
                 <ul className="menu-itens">
-                    <li><a href="#" className="menu-item menu-item--entrar">Entrar</a></li>
-                    <li><a href="#" className="menu-item">Produtos</a></li>
-                    <li><a href="/" className="menu-item">Blog</a></li>
-                    <li><a href="/sobre" className="menu-item">Sobre</a></li>
+                    <li><Link to="#" className="menu-item menu-item--entrar">Entrar</Link></li>
+                    <li><Link to="#" className="menu-item">Produtos</Link></li>
+                    <li><Link to="/" className="menu-item">Blog</Link></li>
+                    <li><Link to="/sobre" className="menu-item">Sobre</Link></li>
                 </ul>
             </nav>
             <div className="menu-cabecalho-background"></div>
---
+++ src/components/ListaPost.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {busca} from '../api/api'
+const ListaPost = () => {
+    return (
+        <section className="posts container">
+
+        </section>
+    )
+}
+
+export default ListaPost
