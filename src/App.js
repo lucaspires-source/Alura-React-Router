324a75baba2253428fd7c7515c580589e0657918
lucaspires-source <pires.lucas94@gmail.com>
2021-04-03 08:54:58 -0300

    Aula 01.05

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React from 'react'
 import './assets/css/base/base.css'
 import Home from './paginas/Home';
 import Sobre from './paginas/Sobre';
-import {BrowserRouter,Route,Link} from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 
 
 const App = () => {
 
   return (
     <BrowserRouter>
-      <Route exact path="/" component={Home}/>
-      <Route path="/sobre" component={Sobre}/>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/sobre" component={Sobre} />
+      </Switch>
     </BrowserRouter>
-  
+
   )
 }
 
